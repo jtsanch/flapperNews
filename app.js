7f72951d20b79423916b9b35cb2f0f17cab6a9d3
Travis Sanchez <johntssanch@yahoo.com>
2015-11-07 23:34:03 -0800

    Adding model elems

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,30 @@ var mongoose = require('mongoose');
 var Post  = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+router.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if(err) { return next(err); }
+		if(!post) { return next(new Error('can\'t find post'));}
+
+		req.post = post;
+		return next();
+	});
+});
+
+router.param('comment', function(req, res, next, id){
+	var query = Comment.findById(id);
+
+	query.exec(function( err, comment){
+		if(err) { return next(err);}
+		if(!comment) { return next(new Error('No comment'));}
+
+		req.comment = comment;
+		return next();
+	})
+});
+
 router.get('/posts', function(req, res, next){
 	Post.find(function(err, posts){
 		res.json(posts);
@@ -13,10 +37,34 @@ router.get('/posts', function(req, res, next){
 
 router.post('/posts', function(req, res, next){
 	var post = new Post(req.body);
+
 	post.save(function(err, post){
 		if(err){ return next(err); }
 		res.json(post);
 	});
+
+});
+
+router.get('/posts/:post', function(req, res){
+	res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next){
+	req.post.upvote(function(err, post){
+		if(err) { return next(err);}
+		res.json(post);
+	});
+});
+
+router.post('/posts/:post/comments', function(req, res, next){
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+	req.post.comments.push(comment);
+
+	comment.save(function(err, comment){
+		if(err) {return next(err);}
+		res.json(comment);
+	})
 });
 
 /* GET home page. */
